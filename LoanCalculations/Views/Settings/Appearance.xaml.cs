e36b330dc2912482f53403caba755d24c0e7f9fa
Rodion <Radium-ON@outlook.com>
2020-06-09 00:11:26 +0700

    Add project files.

---
+++ LoanCalculations/Views/Settings/Appearance.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using LoanCalculations.Pages.Settings;
+
+namespace LoanCalculations.Views.Settings
+{
+    /// <summary>
+    /// Interaction logic for Appearance.xaml
+    /// </summary>
+    public partial class Appearance : UserControl
+    {
+        public Appearance()
+        {
+            InitializeComponent();
+
+            // create and assign the appearance view model
+            this.DataContext = new AppearanceViewModel();
+        }
+    }
+}
