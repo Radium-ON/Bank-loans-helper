60641d7ff0cede14d9482d72e1053c3f89c50a34
Rodion <Radium-ON@outlook.com>
2020-06-15 22:46:00 +0700

    Add OffersFilterViewModel

---
+++ LoanCalculations/ViewModels/OffersFilterViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Commands;
+
+namespace LoanCalculations.ViewModels
+{
+    public class OffersFilterViewModel : ModernViewModelBase
+    {
+        public OffersFilterViewModel()
+        {
+            NavigatingFromCommand = new DelegateCommand(NavigatingFrom);
+            NavigatedFromCommand = new DelegateCommand(NavigatedFrom);
+            NavigatedToCommand = new DelegateCommand(NavigatedTo);
+            FragmentNavigationCommand = new DelegateCommand(FragmentNavigation);
+            LoadedCommand = new DelegateCommand(LoadData);
+            IsVisibleChangedCommand = new DelegateCommand(VisibilityChanged);
+        }
+
+        /// <summary>
+        /// Visibilities the changed.
+        /// </summary>
+        private void VisibilityChanged()
+        {
+            Debug.WriteLine("AllClientsViewModel - VisibilityChanged");
+        }
+
+        /// <summary>
+        /// Loads the data.
+        /// </summary>
+        private void LoadData()
+        {
+            Debug.WriteLine("AllClientsViewModel - LoadData");
+        }
+
+        /// <summary>
+        /// Navigateds from.
+        /// </summary>
+        private void NavigatedFrom()
+        {
+            // called when we navigated to another view
+            Debug.WriteLine("AllClientsViewModel - NavigatedFrom");
+        }
+
+        /// <summary>
+        /// Navigateds to.
+        /// </summary>
+        private void NavigatedTo()
+        {
+            // called when we navigate to the view related with this view model.
+            Debug.WriteLine("AllClientsViewModel - NavigatedTo");
+        }
+
+        /// <summary>
+        /// Fragments the navigation.
+        /// </summary>
+        private void FragmentNavigation()
+        {
+            Debug.WriteLine("AllClientsViewModel - FragmentNavigation");
+        }
+
+        /// <summary>
+        /// Navigatings from.
+        /// </summary>
+        private void NavigatingFrom()
+        {
+            // Called when we will navigate to new view
+            Debug.WriteLine("AllClientsViewModel - NavigatingFrom");
+        }
+    }
+}
