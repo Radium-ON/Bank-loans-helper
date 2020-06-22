9f6e724b3b3ea3e1cde20e0e2fdff100d059b423
Rodion <Radium-ON@outlook.com>
2020-06-22 23:24:26 +0700

    App extended by PrismApp class

--- LoanCalculations/App.xaml.cs
+++ LoanCalculations/App.xaml.cs
@@ -5,13 +5,30 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using CommonServiceLocator;
+using LoanHepler.Views;
+using Prism.Ioc;
+using Prism.Unity;
 
 namespace LoanHepler
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        #region Overrides of PrismApplicationBase
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+        protected override Window CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<MainWindow>();
+        }
+
+        #endregion
     }
 }
