784357fba67fed07f733d72cbd62762dacfee585
Rodion <Radium-ON@outlook.com>
2020-06-24 11:46:52 +0700

    Проверка ввода данных работает для ClientView (но не вся).

--- ClientInfo/Properties/Resources.Designer.cs
+++ ClientInfo/Properties/Resources.Designer.cs
@@ -8,11 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ClientInfo.Properties
-{
+namespace ClientInfo.Properties {
     using System;
-
-
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -20,52 +19,107 @@ namespace ClientInfo.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if (object.ReferenceEquals(resourceMan, null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ClientInfo.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Не заполнен возраст клиента!.
+        /// </summary>
+        internal static string client_error_missing_age {
+            get {
+                return ResourceManager.GetString("client_error_missing_age", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Не заполнено имя клиента!.
+        /// </summary>
+        internal static string client_error_missing_first_name {
+            get {
+                return ResourceManager.GetString("client_error_missing_first_name", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Нет паспортных данных!.
+        /// </summary>
+        internal static string client_error_missing_passport {
+            get {
+                return ResourceManager.GetString("client_error_missing_passport", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Нет данных о зарплате!.
+        /// </summary>
+        internal static string client_error_missing_salary {
+            get {
+                return ResourceManager.GetString("client_error_missing_salary", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Не вписан трудовой стаж!.
+        /// </summary>
+        internal static string client_error_missing_seniority {
+            get {
+                return ResourceManager.GetString("client_error_missing_seniority", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Не заполнена фамилия клиента!.
+        /// </summary>
+        internal static string client_error_missing_surname {
+            get {
+                return ResourceManager.GetString("client_error_missing_surname", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Не заполнен ИНН!.
+        /// </summary>
+        internal static string client_error_missing_tin {
+            get {
+                return ResourceManager.GetString("client_error_missing_tin", resourceCulture);
             }
         }
     }
--- ClientInfo/ViewModels/ClientViewModel.cs
+++ ClientInfo/ViewModels/ClientViewModel.cs
@@ -2,24 +2,215 @@
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClientInfo.Properties;
 
 namespace ClientInfo.ViewModels
 {
-    public class ClientViewModel : BindableBase
+    public class ClientViewModel : BindableBase, IDataErrorInfo
     {
-        private string _message;
-        public string Message
-        {
-            get { return _message; }
-            set { SetProperty(ref _message, value); }
-        }
+        #region Backing Fields
+        private string _firstName;
+        private string _surName;
+        private string _passport;
+        private string _tin;
+        private int _age;
+        private int _seniority;
+        private decimal _salary;
 
+        #endregion
         public ClientViewModel()
         {
-            Message = "View A from your Prism Module";
         }
+
+        #region Bindable Props
+
+        public string FirstName
+        {
+            get => _firstName;
+            set => SetProperty(ref _firstName, value);
+        }
+        public string Surname
+        {
+            get => _surName;
+            set => SetProperty(ref _surName, value);
+        }
+        public string Passport
+        {
+            get { return _passport; }
+            set { SetProperty(ref _passport, value); }
+        }
+        public string TIN
+        {
+            get { return _tin; }
+            set { SetProperty(ref _tin, value); }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set { SetProperty(ref _age, value); }
+        }
+        public int Seniority
+        {
+            get { return _seniority; }
+            set { SetProperty(ref _seniority, value); }
+        }
+        public decimal Salary
+        {
+            get { return _salary; }
+            set { SetProperty(ref _salary, value); }
+        }
+        #endregion
+
+        #region Implementation of IDataErrorInfo
+
+        public string this[string columnName] => GetValidationError(columnName);
+
+        public string Error { get => null; }
+
+        #endregion
+
+        #region Validation
+
+        public bool IsValid
+        {
+            get
+            {
+                foreach (string property in _validatedProperties)
+                    if (GetValidationError(property) != null)
+                        return false;
+
+                return true;
+            }
+        }
+
+        static readonly string[] _validatedProperties =
+        {
+            "FirstName",
+            "LastName",
+            "Passport",
+            "TIN",
+            "Age",
+            "Seniority",
+            "Salary"
+        };
+
+
+
+        string GetValidationError(string propertyName)
+        {
+            if (Array.IndexOf(_validatedProperties, propertyName) < 0)
+                return null;
+
+            string error = null;
+
+            switch (propertyName)
+            {
+                case "FirstName":
+                    error = ValidateFirstName();
+                    break;
+
+                case "Surname":
+                    error = ValidateSurName();
+                    break;
+
+                case "Passport":
+                    error = ValidatePassport();
+                    break;
+
+                case "TIN":
+                    error = ValidateTin();
+                    break;
+
+                case "Age":
+                    error = ValidateAge();
+                    break;
+
+                case "Seniority":
+                    error = ValidateSeniority();
+                    break;
+
+                case "Salary":
+                    error = ValidateSalary();
+                    break;
+            }
+
+            return error;
+        }
+
+        private string ValidateSalary()
+        {
+            if (Salary<=0)
+            {
+                return Resources.client_error_missing_salary;
+            }
+            return null;
+        }
+
+        private string ValidateSeniority()
+        {
+            if (Seniority>100)
+            {
+                return Resources.client_error_missing_seniority;
+            }
+            return null;
+        }
+
+        private string ValidateAge()
+        {
+            if (Age==0)
+            {
+                return Resources.client_error_missing_age;
+            }
+            return null;
+        }
+
+        private string ValidateTin()
+        {
+            if (IsStringMissing(TIN))
+            {
+                return Resources.client_error_missing_tin;
+            }
+            return null;
+        }
+
+        private string ValidatePassport()
+        {
+            if (IsStringMissing(Passport))
+            {
+                return Resources.client_error_missing_passport;
+            }
+            return null;
+        }
+
+        private string ValidateFirstName()
+        {
+            if (IsStringMissing(FirstName))
+            {
+                return Resources.client_error_missing_first_name;
+            }
+            return null;
+        }
+
+        private string ValidateSurName()
+        {
+
+            if (IsStringMissing(Surname))
+                return Resources.client_error_missing_surname;
+
+            return null;
+        }
+
+        private static bool IsStringMissing(string value)
+        {
+            return
+                string.IsNullOrEmpty(value) ||
+                value.Trim() == string.Empty;
+        }
+        #endregion
     }
 }
