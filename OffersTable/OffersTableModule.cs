12dd916c6a6934a571183a8bf1e8db45b41bd4a1
Rodion <Radium-ON@outlook.com>
2020-07-02 11:58:40 +0700

    Новый модуль OffersTable

---
+++ OffersTable/OffersTableModule.cs
@@ -0,0 +1,20 @@
+﻿using OffersTable.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace OffersTable
+{
+    public class OffersTableModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
