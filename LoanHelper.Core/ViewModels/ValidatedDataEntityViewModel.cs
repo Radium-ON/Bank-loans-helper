d80ccaa999ac314682794153f839b2d2faed7933
Rodion <Radium-ON@outlook.com>
2020-07-20 10:39:16 +0700

    Новый абстрактный класс будут наследовать ViewModels каждой сущности. По умолчанию есть реализация валидации, но нет свойств привязки.

---
+++ LoanHelper.Core/ViewModels/ValidatedDataEntityViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace LoanHelper.Core.ViewModels
+{
+    public abstract class ValidatedDataEntityViewModel<TEntity> : IDataErrorInfo
+    {
+        #region Backing Fields
+
+        private protected readonly TEntity _entity;
+
+        private readonly string[] _validatedProperties;
+
+        #endregion
+
+        protected ValidatedDataEntityViewModel(TEntity entity, string[] validatedProperties)
+        {
+            _entity = entity;
+            _validatedProperties = validatedProperties;
+        }
+
+        public TEntity Entity => _entity;
+
+        public bool IsUnique => !CheckIsEntityContainsInContext(_entity);
+
+        protected abstract bool CheckIsEntityContainsInContext(TEntity entity);
+
+
+
+        #region Implementation of IDataErrorInfo
+
+        public string this[string columnName] => GetValidationError(columnName);
+
+        public string Error => null;
+
+        #endregion
+
+        #region Validation
+
+        public bool IsValid
+        {
+            get
+            {
+                return _validatedProperties.All(property => GetValidationError(property) == null);
+            }
+        }
+
+        public string[] ValidatedProperties => _validatedProperties;
+
+
+
+        protected virtual string GetValidationError(string propertyName)
+        {
+            if (Array.IndexOf(_validatedProperties, propertyName) < 0)
+                return null;
+
+            return null;
+        }
+
+
+
+        private static bool IsStringMissing(string value)
+        {
+            return
+                string.IsNullOrEmpty(value) ||
+                value.Trim() == string.Empty;
+        }
+
+        private static bool IsStringAllDigits(string value)
+        {
+            return value.All(char.IsDigit);
+        }
+
+        private static bool IsStringAllLetters(string value)
+        {
+            return value.All(char.IsLetter);
+        }
+        #endregion
+    }
+}
